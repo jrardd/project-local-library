49372b6986867e8332584b6a8a829e2a788cbec2
jrardd <jrardd@gmail.com>
2020-12-16 08:31:57 -0800

    completed accounts.js and books.js functions

--- public/src/accounts.js
+++ public/src/accounts.js
@@ -1,10 +1,40 @@
-function findAccountById(accounts, id) {}
+function findAccountById(accounts, id) {
+  return accounts.find((account) => account.id === id);
+}
 
-function sortAccountsByLastName(accounts) {}
+function sortAccountsByLastName(accounts) {
+  return accounts.sort((account1, account2) =>
+    account1.name.last > account2.name.last ? 1 : -1
+  );
+}
 
-function numberOfBorrows(account, books) {}
+function numberOfBorrows(account, books) {
+  return books.reduce((count, book) => {
+    const borrowIDs = book.borrows.map((borrow) => borrow.id);
+    return borrowIDs.includes(account.id) ? count + 1 : count;
+  }, 0);
+}
 
-function getBooksPossessedByAccount(account, books, authors) {}
+function getBooksPossessedByAccount(account, books, authors) {
+  let possessedBooks = [];
+
+  for (let i = 0; i < books.length; i++) {
+    let book = books[i];
+    const { id, title, genre, borrows } = book;
+    for (let j = 0; j < borrows.length; j++) {
+      if (borrows[j].id === account.id && borrows[j].returned === false) {
+        for (let k = 0; k < authors.length; k++) {
+          let author = authors[k];
+          if (author.id === book.authorId) {
+            let tempBook = { id, title, genre, author, borrows };
+            possessedBooks.push(tempBook);
+          }
+        }
+      }
+    }
+  }
+  return possessedBooks;
+}
 
 module.exports = {
   findAccountById,
--- public/src/books.js
+++ public/src/books.js
@@ -1,10 +1,39 @@
-function findAuthorById(authors, id) {}
+function findAuthorById(authors, id) {
+  return authors.find((author) => author.id === id);
+}
 
-function findBookById(books, id) {}
+function findBookById(books, id) {
+  return books.find((book) => book.id === id);
+}
 
-function partitionBooksByBorrowedStatus(books) {}
+function partitionBooksByBorrowedStatus(books) {
+  let borrowedBooks = books.filter((book) => {
+    return book.borrows[0].returned == false;
+  });
+  let returnedBooks = books.filter((book) => {
+    return book.borrows[0].returned == true;
+  });
+  return [borrowedBooks, returnedBooks];
+}
 
-function getBorrowersForBook(book, accounts) {}
+function getBorrowersForBook(book, accounts) {
+  let transactions = [];
+  let borrows = book.borrows.map((borrow) => ({
+    id: borrow.id,
+    returned: borrow.returned,
+  }));
+
+  for (let i in borrows) {
+    const borrow = borrows[i];
+    const account = accounts.find((account) => borrow.id === account.id);
+    transactions.push({
+      ...borrow,
+      ...account,
+    });
+  }
+
+  return transactions.slice(0, 10);
+}
 
 module.exports = {
   findAuthorById,
